87bc6b8de681ca36fc14953120baea2c302fda47
giridharan-glitch <giridharann.m@gmail.com>
2026-06-07 23:49:49 +0530

    Add study session feature

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("Study Tracker")
+print("Study Tracker")
+
+study_data = []
+
+def add_session():
+    subject = input("Enter subject: ")
+    hours = float(input("Enter study hours: "))
+
+    session = {
+        "subject": subject,
+        "hours": hours
+    }
+
+    study_data.append(session)
+
+    print("Session added successfully!")
+
+add_session()
+
+print(study_data)
